815205e4c1dec49bab119a962617355a70977b09
KopylashTetiana <kopylashtetiana@gmail.com>
2023-04-03 21:39:09 +0300

    DoubleInteger task (CodeUnderPressure class) added.

---
+++ src/Tasks8kyu/CodeUnderPressure.java
@@ -0,0 +1,13 @@
+package Tasks8kyu;
+/*
+Code as fast as you can! You need to double the integer and return it.
+ */
+public class CodeUnderPressure {
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(2));
+    }
+}
