ea9c9506dd0031317bc87514301a22e331eb031e
KopylashTetiana <kopylashtetiana@gmail.com>
2023-04-03 21:51:34 +0300

    CalculateBMI task added.

---
+++ src/Tasks8kyu/CalculateBMI.java
@@ -0,0 +1,17 @@
+package Tasks8kyu;
+/*
+Write function bmi that calculates body mass index (bmi = weight / height2).
+if bmi <= 18.5 return "Underweight"
+if bmi <= 25.0 return "Normal"
+if bmi <= 30.0 return "Overweight"
+if bmi > 30 return "Obese"
+ */
+public class CalculateBMI {
+    public static String bmi(double weight, double height) {
+        double bmi = weight / (height * height);
+        return (bmi <= 18.5) ? "Underweight" : (bmi <= 25.0) ? "Normal": (bmi <= 30) ? "Overweight" : "Obese";
+    }
+    public static void main (String[] args) {
+        System.out.println(bmi(49.8, 1.64));
+    }
+}
