147d0d03ddec694277cee1fa5521a25e82086c85
KopylashTetiana <kopylashtetiana@gmail.com>
2023-04-14 12:21:26 +0300

    Squaring task added.

---
+++ src/main/java/Tasks8kyu/Squaring.java
@@ -0,0 +1,16 @@
+package main.java.Tasks8kyu;
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+ */
+public class Squaring {
+    public static int square(int n){
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(square(10));
+        System.out.println(square(1));
+        System.out.println(square(3));
+        System.out.println(square(0));
+    }
+}
