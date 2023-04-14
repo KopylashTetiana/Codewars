2985bf1d2ded7871632356201695fec43d803da6
KopylashTetiana <kopylashtetiana@gmail.com>
2023-04-14 20:21:57 +0300

    ValidParentheses task added.

---
+++ src/main/java/Tasks7kyu/ValidParentheses.java
@@ -0,0 +1,38 @@
+package main.java.Tasks7kyu;
+/*
+DESCRIPTION:
+Write a function that takes a string of parentheses, and determines if the order
+of the parentheses is valid. The function should return true if the string is
+valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+Constraints
+0 <= length of input <= 100
+
+All inputs will be strings, consisting only of characters ( and ).
+Empty strings are considered balanced (and therefore valid), and will be tested.
+For languages with mutable strings, the inputs should not be mutated.
+
+Protip: If you are trying to figure out why a string of parentheses is invalid,
+paste the parentheses into the code editor, and let the code highlighting show you!
+ */
+public class ValidParentheses {
+    public static boolean validParentheses(String parenStr) {
+        while (parenStr.contains("()")) {
+            parenStr = parenStr.replace("()", "");
+        }
+        return parenStr.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(validParentheses("()"));
+        System.out.println(validParentheses(")(()))"));
+        System.out.println(validParentheses("("));
+        System.out.println(validParentheses("(())((()())())"));
+        System.out.println(validParentheses("(()())"));
+    }
+}
